ba62609c92e5b8c16e4dd24e0e7731f5bf354113
RR David <raphaelruizd@gmail.com>
2020-03-21 18:11:12 +1100

    Updates

---
+++ precache-manifest.e331b3c8cc6d2d0daeee5305259fdafc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40cd8d444fc1b1ecf89260845afd976e",
+    "url": "/digimons/index.html"
+  },
+  {
+    "revision": "b42489a43cc67748f947",
+    "url": "/digimons/static/css/main.330ea27f.chunk.css"
+  },
+  {
+    "revision": "241339d833e870fcbefe",
+    "url": "/digimons/static/js/2.11f897eb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/digimons/static/js/2.11f897eb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b42489a43cc67748f947",
+    "url": "/digimons/static/js/main.92da74ef.chunk.js"
+  },
+  {
+    "revision": "1df5a674271729784904",
+    "url": "/digimons/static/js/runtime-main.34a214b0.js"
+  }
+]);
